8f17e98ffc498b76a81f8ae40437dfa14eeb1732
Jayvious <JayviousWilliams@gmail.com>
2019-09-27 01:21:48 -0400

    Finihing up the AppleTree and Basket

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -13,6 +13,30 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Get the current screen position of the mouse form Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The Camera's position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x positions of this Basket to this x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //Find out what hit this basket
+        GameObject collideWith = coll.gameObject;
+        if(collideWith.tag == "Apple")
+        {
+            Destroy(collideWith);
+        }
+    }
+
 }
