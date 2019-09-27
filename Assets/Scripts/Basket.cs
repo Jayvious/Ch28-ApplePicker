ac27bd6e86387758590516eeb5e36bf9924a489a
Jayvious <JayviousWilliams@gmail.com>
2019-09-27 16:51:52 -0400

    More C# code stuff

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //This allows use of uGUI features.
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGo = GameObject.Find("ScoreCounter");
+        //Get the Text Component of that GameObject
+        scoreGT = scoreGo.GetComponent<Text>();
+        //Set the stating number of points to 0
+        scoreGT.text = "0";
+
     }
 
     // Update is called once per frame
@@ -37,6 +46,12 @@ public class Basket : MonoBehaviour
         {
             Destroy(collideWith);
         }
+        //Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+        //Add points for catching the Apple
+        score += 100;
+        //Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
     }
 
 }
