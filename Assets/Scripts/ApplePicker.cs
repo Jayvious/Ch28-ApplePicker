6de4d6dfbab323f5284bf18ceda1892a0b0d059f
Jayvious <JayviousWilliams@gmail.com>
2019-09-27 17:34:08 -0400

    Finishing up the prototype

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,19 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for(int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
 
 
@@ -38,5 +42,14 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGo);
         }
+
+        int basketIndex = basketList.Count - 1;
+
+        GameObject tBasketGO = basketList[basketIndex];
+
+        basketList.RemoveAt(basketIndex);
+
+        Destroy(tBasketGO);
+
     }
 }
